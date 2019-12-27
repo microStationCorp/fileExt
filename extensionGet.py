0b871cba3fa732942577f2bfb23e0b474b12dc5c
root <sujanmondal916@gmail.com>
2019-12-27 16:19:40 +0530

    all set....final commit

--- extensionGet.py
+++ extensionGet.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import os
 
 
 def getExtList(*url):
@@ -12,16 +13,38 @@ def getExtList(*url):
         for table in tableList:
             spanList = table.find_all('span')
             for span in spanList:
-                text = '.' + span.text.lower()
-                extList.add(text)
+                # text = '.' + span.text.lower()
+                if span.text.find(' ') == -1:
+                    text = '.' + span.text.lower()
+                    extList.add(text)
+                else:
+                    minilist = span.text.split(' ')
+                    for i in minilist:
+                        if i.endswith(','):
+                            i = i[:-1]
+                        elif i.endswith('..') or i == '':
+                            continue
+                        text = '.' + i
+                        extList.add(text)
 
     return list(extList)
 
-urlsym='https://en.wikipedia.org/wiki/List_of_filename_extensions'
+
+urlsym = 'https://en.wikipedia.org/wiki/List_of_filename_extensions'
 urlae = "https://en.wikipedia.org/wiki/List_of_filename_extensions_(A%E2%80%93E)"
 urlfl = "https://en.wikipedia.org/wiki/List_of_filename_extensions_(F%E2%80%93L)"
 urlmr = "https://en.wikipedia.org/wiki/List_of_filename_extensions_(M%E2%80%93R)"
 urlsz = "https://en.wikipedia.org/wiki/List_of_filename_extensions_(S%E2%80%93Z)"
-ext=getExtList(urlsym, urlae, urlfl, urlmr, urlsz)
+ext = getExtList(urlsym, urlae, urlfl, urlmr, urlsz)
 ext.sort()
-print(type(ext),len(ext))
+print(len(ext))
+try:
+    file = open('extension.txt', 'x')
+except:
+    os.remove('extension.txt')
+
+with open('extension.txt', 'a') as file:
+    for i in ext:
+        file.write(f'{i}\n')
+
+file.close()
